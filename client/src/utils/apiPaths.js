12788260fe25e00f613c307e2d42031861efe3ba
Rohit Sharma <rohits8731@gmail.com>
2026-03-18 13:04:51 +0530

    Create apiPaths.js

---
+++ client/src/utils/apiPaths.js
@@ -0,0 +1,30 @@
+const API_BASE_URL = `${import.meta.env.VITE_SERVER_URL}/api` || "http://localhost:3000/api";
+
+export const BASE_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
+
+export const API_PATHS = {
+    AUTH: {
+        REGISTER: `${API_BASE_URL}/auth/register`,
+        LOGIN: `${API_BASE_URL}/auth/login`,
+        GOOGLE_LOGIN: `${API_BASE_URL}/auth/google`,
+        GET_PROFILE: `${API_BASE_URL}/auth/profile/:id`,
+        UPDATE_PROFILE: `${API_BASE_URL}/auth/profile/:id`,
+        GET_ALL_PROFILES: `${API_BASE_URL}/auth/profiles`,
+    },
+    BOOKS: {
+        GET_ALL: `${API_BASE_URL}/books`,
+        GET_BY_ID: `${API_BASE_URL}/books/:id`,
+        CREATE: `${API_BASE_URL}/books`,
+        UPDATE: `${API_BASE_URL}/books/:id`,
+        DELETE: `${API_BASE_URL}/books/:id`,
+        UPLOAD_COVER: `${API_BASE_URL}/books/cover/:id`,
+    },
+    AI: {
+        GENERATE_OUTLINE: `${API_BASE_URL}/ai/generate-book-outline`,
+        GENERATE_CONTENT: `${API_BASE_URL}/ai/generate-book-content`,
+    },
+    EXPORT: {
+        EXPORT_PDF: `${API_BASE_URL}/export/pdf/:id`,
+        EXPORT_DOCX: `${API_BASE_URL}/export/docx/:id`,
+    },
+};
